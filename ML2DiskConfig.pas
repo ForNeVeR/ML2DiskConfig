2b27f0d3203ea0f9a9c36954fb27cc58650ea6a2
Friedrich von Never <friedrich@fornever.me>
2017-04-04 00:04:41 +0700

    #6: remove outdated build files
    We're now building the project using scripts and don't rely on
    PascalABC.NET IDE.

--- ML2DiskConfig.pas
+++
@@ -1,7 +0,0 @@
-uses Main;
-
-begin
-  System.Windows.Forms.Application.EnableVisualStyles();
-  System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-  System.Windows.Forms.Application.Run(new MainForm)
-end.
